af2dbc204643dc1f40fce41e4670cfcfb67329b9
John Squibb <git@johnsquibb.com>
2021-06-04 13:15:14 +0000

    Adding more transpile tests

--- test/TranspilerTest.php
+++ test/TranspilerTest.php
@@ -2,7 +2,9 @@
 
 namespace AdventureGameMarkupLanguage;
 
+use AdventureGameMarkupLanguage\Hydrator\ContainerEntityHydrator;
 use AdventureGameMarkupLanguage\Hydrator\ItemEntityHydrator;
+use AdventureGameMarkupLanguage\Hydrator\LocationEntityHydrator;
 use PHPUnit\Framework\TestCase;
 
 class TranspilerTest extends TestCase
@@ -51,6 +53,22 @@ class TranspilerTest extends TestCase
         
         [description]
         A small brass key that goes to a heavy wooden door.
+        
+        [LOCATION]
+        id=theVastExpanse
+        name=The Vast Expanse
+        [description]
+        A seemingly ever-sprawling expanse of nothingness.
+        It goes on for miles and miles, well beyond your line of sight.
+        
+        [CONTAINER]
+        id=theTreasureChest
+        name=A Treasure Chest
+        locked=yes
+        key=keyToTreasureChest
+        [description]
+        A chest containing valuable items.
+        It appears to be locked.
         END;
 
         $lexer = new Lexer();
@@ -59,10 +77,18 @@ class TranspilerTest extends TestCase
 
         $hydrators = $transpiler->transpile($fixture);
 
-        $this->assertCount(2, $hydrators);
+        $this->assertCount(4, $hydrators);
+
         $this->assertInstanceOf(ItemEntityHydrator::class, $hydrators[0]);
         $this->assertEquals('flashlight', $hydrators[0]->getId());
+
         $this->assertInstanceOf(ItemEntityHydrator::class, $hydrators[1]);
         $this->assertEquals('keyToWoodenDoor', $hydrators[1]->getId());
+
+        $this->assertInstanceOf(LocationEntityHydrator::class, $hydrators[2]);
+        $this->assertEquals('theVastExpanse', $hydrators[2]->getId());
+
+        $this->assertInstanceOf(ContainerEntityHydrator::class, $hydrators[3]);
+        $this->assertEquals('theTreasureChest', $hydrators[3]->getId());
     }
 }
