3c73e5ae1491fc9c8d9782fb092ed29640718f47
John Squibb <git@johnsquibb.com>
2021-06-17 14:57:30 +0000

    Adding item, location hooks to event

--- src/Hydrator/AbstractEventHydrator.php
+++ src/Hydrator/AbstractEventHydrator.php
@@ -6,6 +6,8 @@ abstract class AbstractEventHydrator extends AbstractEntityAssignmentHydrator
 {
     private string $type = '';
     private string $trigger = '';
+    private string $item = '';
+    private string $location = '';
 
     public function assign(string $variable, array $values): void
     {
@@ -16,9 +18,25 @@ abstract class AbstractEventHydrator extends AbstractEntityAssignmentHydrator
             case 'trigger':
                 $this->trigger = $this->firstValue($values);
                 break;
+            case 'item':
+                $this->item = $this->firstValue($values);
+                break;
+            case 'location':
+                $this->location = $this->firstValue($values);
+                break;
             default:
                 parent::assign($variable, $values);
         }
+    }
+
+    public function getItem(): string
+    {
+        return $this->item;
+    }
+
+    public function getLocation(): string
+    {
+        return $this->location;
     }
 
     public function getType(): string
